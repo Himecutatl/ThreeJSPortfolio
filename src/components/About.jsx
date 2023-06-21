72a60f8ba7eca9c81b83bf7cd633500d0ade3cf1
Himecutatl <corey.kim518@gmail.com>
2023-06-21 02:40:22 -0400

    updated exp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,8 +47,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript. Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in React and
+        JavaScript. Let's collaborate!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
